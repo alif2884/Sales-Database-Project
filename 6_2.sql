48d12214be4a2a3c79d3552dbb3bcbe5b32e6e90
Ali Fakhar <alifakhar756@gmail.com>
2025-07-28 22:46:28 +0330

    Delete 6_2.sql

--- 6_2.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    CASE 
-        WHEN od.order_discount > 0 THEN 'With Discount'
-        ELSE 'Without Discount'
-    END AS discount_category,
-    SUM(od.order_quantity) AS total_quantity_sold
-FROM order_details1 od
-GROUP BY discount_category;
