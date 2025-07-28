affde839e981d9c3fd36c64f9825fb8b9f3d542b
Ali Fakhar <alifakhar756@gmail.com>
2025-07-28 22:47:45 +0330

    Delete 5_2.sql

--- 5_2.sql
+++
@@ -1,7 +0,0 @@
-SELECT p.product_id, p.product_name, s.company_name AS supplier_name, c.category_name,
-       ROUND(p.unit_price::NUMERIC, 2) AS unit_price
-FROM products1 p
-JOIN suppliers1 s ON p.supplier_id = s.supplier_id
-JOIN categories1 c ON p.category_id = c.category_id
-WHERE p.unit_price > (SELECT AVG(unit_price) FROM products1)
-ORDER BY p.unit_price DESC;
