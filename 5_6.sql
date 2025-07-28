fe73b46ef454e87d28516d665807dbcfa2ab0ef7
Ali Fakhar <alifakhar756@gmail.com>
2025-07-28 22:48:43 +0330

    Delete 5_6.sql

--- 5_6.sql
+++
@@ -1,5 +0,0 @@
-SELECT p.product_id, p.product_name, s.company_name AS supplier_name, c.category_name
-FROM products1 p
-LEFT JOIN suppliers1 s ON p.supplier_id = s.supplier_id
-LEFT JOIN categories1 c ON p.category_id = c.category_id
-ORDER BY p.product_name;
