d87a6437f2279d2b02a805f945578743787fa61b
Ali Fakhar <alifakhar756@gmail.com>
2025-07-28 22:45:40 +0330

    Delete 5_3.sql

--- 5_3.sql
+++
@@ -1,8 +0,0 @@
-SELECT o.order_id, 
-       ROUND(SUM(od.order_price * od.order_quantity * (1 - od.order_discount))::NUMERIC, 2) AS total_amount,
-       o.order_date,
-       o.required_date
-FROM orders1 o
-JOIN order_details1 od ON o.order_id = od.order_id
-GROUP BY o.order_id, o.order_date, o.required_date
-ORDER BY o.order_id;
