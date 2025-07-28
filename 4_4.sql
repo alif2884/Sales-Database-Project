363232611fea43c37e899b7f12b3c46ef4a033a9
Ali Fakhar <alifakhar756@gmail.com>
2025-07-28 22:47:33 +0330

    Delete 4_4.sql

--- 4_4.sql
+++
@@ -1,5 +0,0 @@
-SELECT o.order_id, o.customer_id, o.order_date, a.country
-FROM orders1 o
-JOIN customers1 c ON o.customer_id = c.customer_id
-JOIN addresses1 a ON c.address_id = a.address_id
-WHERE a.country = 'Germany';
