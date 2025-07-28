af6f48eb52085e86314d19aa9ad4bb3bd36cce28
Ali Fakhar <alifakhar756@gmail.com>
2025-07-28 22:44:16 +0330

    Delete 5_1.sql

--- 5_1.sql
+++
@@ -1,6 +0,0 @@
-SELECT e.first_name, e.last_name, COUNT(o.order_id) AS order_count
-FROM employees1 e
-LEFT JOIN orders1 o ON e.employee_id = o.employee_id
-GROUP BY e.employee_id, e.first_name, e.last_name
-HAVING COUNT(o.order_id) > 15
-ORDER BY order_count DESC;
