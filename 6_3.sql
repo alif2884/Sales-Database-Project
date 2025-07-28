deac637b2dfe43eb1b919f6d92c735d90f1fcdc5
Ali Fakhar <alifakhar756@gmail.com>
2025-07-28 22:46:15 +0330

    Delete 6_3.sql

--- 6_3.sql
+++
@@ -1,7 +0,0 @@
-SELECT c.customer_id, c.company_name,
-       ROUND(SUM(od.order_price * od.order_quantity * (1 - od.order_discount))::NUMERIC, 2) AS total_order_value
-FROM customers1 c
-LEFT JOIN orders1 o ON c.customer_id = o.customer_id
-LEFT JOIN order_details1 od ON o.order_id = od.order_id
-GROUP BY c.customer_id, c.company_name
-ORDER BY total_order_value DESC;
